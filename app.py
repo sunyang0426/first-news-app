2198a144bc221376b7e31a855d459e85d8686363
admin <admin@admins-Mac-Pro-3.local>
2017-03-04 15:25:41 -0500

    Created basic table

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
+import csv
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
 
+def get_csv():
+	csv_path = "static/la-riots-deaths.csv"
+	csv_file = open(csv_path, "rb")
+	csv_obj = csv.DictReader(csv_file)
+	csv_list = list(csv_obj)
+	return csv_list
+
+
 @app.route("/")
 def index():
 	template = "index.html"
-	return render_template(template)
+	object_list = get_csv()
+	return render_template(template, object_list = object_list)
 
 
 # If this script is run from the commond line
