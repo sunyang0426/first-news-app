c1f9360081877d30acd4f4a28d5bfa3b623da2fe
admin <admin@admins-Mac-Pro-3.local>
2017-03-04 16:22:47 -0500

    added more details

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import csv
+from flask import abort
 from flask import Flask
 from flask import render_template
 app = Flask(__name__)
@@ -16,6 +17,17 @@ def index():
 	template = "index.html"
 	object_list = get_csv()
 	return render_template(template, object_list = object_list)
+
+
+@app.route("/<row_id>/")
+def detail(row_id):
+	template = "detail.html"
+	object_list = get_csv()
+	for row in object_list:
+		if row["id"] == row_id:
+			return render_template(template, object=row)
+	abort(404)
+
 
 
 # If this script is run from the commond line
